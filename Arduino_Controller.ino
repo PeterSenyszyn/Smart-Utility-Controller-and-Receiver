55fda47530ee77b9dea60765a9c3f9ac0ef5cb8a
Peter Senysyzn <petergsenyszyn@gmail.com>
2015-10-25 21:00:41 -0500

    v1.1
    Cleaned up some code and renamed project files

--- Arduino_Controller.ino
+++ Arduino_Controller.ino
@@ -73,18 +73,10 @@ void setup()
     #endif
 
     // initialize serial communication
-    // (115200 chosen because it is required for Teapot Demo output, but it's
-    // really up to you depending on your project)
     Serial.begin(9600);
     while (!Serial); // wait for Leonardo enumeration, others continue immediately
     
     mySerial.begin(9600);
-
-    // NOTE: 8MHz or slower host processors, like the Teensy @ 3.3v or Ardunio
-    // Pro Mini running at 3.3v, cannot handle this baud rate reliably due to
-    // the baud timing being too misaligned with processor ticks. You must use
-    // 38400 or slower in these cases, or use some kind of external separate
-    // crystal solution for the UART timer.
 
     // initialize device
     Serial.println(F("Initializing I2C devices..."));
@@ -96,15 +88,12 @@ void setup()
 
     // wait for ready
     Serial.println(F("\nSend any character to begin DMP programming and demo: "));
-    //while (Serial.available() && Serial.read()); // empty buffer
-    //while (!Serial.available());                 // wait for data
-    //while (Serial.available() && Serial.read()); // empty buffer again
 
     // load and configure the DMP
     Serial.println(F("Initializing DMP..."));
     devStatus = mpu.dmpInitialize();
 
-    // supply your own gyro offsets here, scaled for min sensitivity
+    //calibrated accelerometer and gyroscope offsets
     mpu.setXAccelOffset(-1806);
     mpu.setYAccelOffset(919);
     mpu.setZAccelOffset(1013);
@@ -143,8 +132,7 @@ void setup()
         Serial.println(F(")"));
     }
 
-    pinMode(AO_pin, INPUT);
-    //pinMode(DO_pin, INPUT);
+    pinMode(AO_pin, INPUT);;
 }
 
 
@@ -160,12 +148,6 @@ void loop()
       
       delay(200);
     }
-    
-    if (enabled)
-      Serial.println("Enabled!");
-      
-    else 
-      Serial.println("Disabled!");
       
     sound = analogRead(AO_pin);
     
